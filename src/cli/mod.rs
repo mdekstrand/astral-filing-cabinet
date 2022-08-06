7dba01ca9a6aa85b895d4c0e29fb00b726ca48f4
Michael Ekstrand <md@ekstrandom.net>
2022-08-05 20:47:48 -0600

    introduce tokio

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -6,6 +6,7 @@
 //! a log backend, and hands control off to the CLI.
 use structopt::StructOpt;
 use anyhow::Result;
+use tokio::runtime::Builder;
 
 /// Manage large data files through attached pointer files committed to VCS.
 #[derive(StructOpt, Debug)]
@@ -15,7 +16,19 @@ pub struct AFC {
 
 impl AFC {
   /// Run the configured AFC command.
+  ///
+  /// This constructs a new [tokio::runtime::Runtime] and uses it to call
+  /// [AFC::invoke_async()].
   pub fn invoke(&self) -> Result<()> {
+    let runtime = Builder::new_multi_thread().enable_all().build()?;
+    runtime.block_on(self.invoke_async())
+  }
+
+  /// Run the configured AFC command asynchronously.
+  ///
+  /// This method is used in code that is setting up its own
+  /// [tokio::runtime::Runtime] and wants to run a task.
+  pub async fn invoke_async(&self) -> Result<()> {
     Ok(())
   }
 }
