02bbb5adde959d8868e4f71009a14b2ce3d54f6c
Michael Ekstrand <md@ekstrandom.net>
2022-08-05 19:50:06 -0600

    Only use anyhow in CLI

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 //! Manage large data files through pointer files stored in Git.
 pub mod cache;
-pub mod cli;
 pub mod remote;
 pub mod tree;
 pub mod settings;
+
+#[cfg(feature="cli")]
+pub mod cli;
