988b09bdd221d69cf3f1d25bea3707f37d5bc0a6
Michael Ekstrand <md@ekstrandom.net>
2022-08-10 09:04:26 -0600

    add test for reading an artifact

--- src/tree/artifact.rs
+++ src/tree/artifact.rs
@@ -1,11 +1,26 @@
 //! Representation for in-tree artifacts.
+use relative_path::{RelativePathBuf, RelativePath};
 
-use relative_path::RelativePathBuf;
+use super::pointer::AFCPointer;
 
 /// An artifact in the work tree.
 pub struct Artifact {
-  /// The path to this artifact within the work tree.
+  /// The path to this artifact relative to the pointer file.
   path: RelativePathBuf,
   /// The path to this artifact's pointer file within the work tree.
-  pointer: RelativePathBuf,
+  pointer_path: RelativePathBuf,
+  /// The artifact pointer data
+  pointer: AFCPointer,
+}
+
+impl Artifact {
+  /// Get the path of this artifact, relative to the pointer file.
+  pub fn path(&self) -> &RelativePath {
+    self.path.as_relative_path()
+  }
+
+  /// Get the path of the pointer file.
+  pub fn pointer_path(&self) -> &RelativePath {
+    self.pointer_path.as_relative_path()
+  }
 }
--- tests/work-tree-listing.rs
+++ tests/work-tree-listing.rs
@@ -21,3 +21,15 @@ async fn test_empty_repo() {
   let arts: Vec<_> = arts.collect().await;
   assert_eq!(arts.len(), 0);
 }
+
+#[tokio::test]
+async fn test_single_artifact() {
+  let dir = TestDir::tarball("single-artifact");
+  let tree = WorkTree::open(dir.path());
+  let arts = tree.scan_artifacts().await;
+  let arts: Vec<_> = arts.collect().await;
+  assert_eq!(arts.len(), 1);
+  let art = &arts[0];
+  assert_eq!(art.path().as_str(), "artifact.dat");
+  assert_eq!(art.pointer_path().as_str(), "artifact.dat.afc");
+}
