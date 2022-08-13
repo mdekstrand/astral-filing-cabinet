bccdb5379404324a82862de9425db3029e7e58bc
Michael Ekstrand <md@ekstrandom.net>
2022-08-13 11:07:02 -0600

    test tree walking

--- src/tree/walk.rs
+++ src/tree/walk.rs
@@ -6,14 +6,19 @@
 use std::collections::LinkedList;
 use std::path::Path;
 use std::io;
-use std::fs::{DirEntry, read_dir, ReadDir, FileType};
+use std::fs::{DirEntry, read_dir};
 
 use log::*;
 
 use futures::Stream;
-use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver};
+use tokio::sync::mpsc::{unbounded_channel};
 use tokio::task::spawn_blocking;
 use tokio_stream::wrappers::UnboundedReceiverStream;
+
+#[cfg(test)]
+use relative_path::RelativePathBuf;
+#[cfg(test)]
+use futures::StreamExt;
 
 /// Recursively walk a directory.
 ///
@@ -66,4 +71,25 @@ pub fn walk_directory<P: AsRef<Path>>(path: P) -> impl Stream<Item=io::Result<Di
   });
 
   UnboundedReceiverStream::new(recv)
+}
+
+
+/// Test walking the source tree, just for basic functionality.
+#[tokio::test]
+async fn test_walk_source() {
+  let mut stream = walk_directory("src");
+  let mut files = Vec::new();
+
+  while let Some(de) = stream.next().await {
+    let de = de.expect("directory entry failure");
+    let ft = de.file_type().expect("file type failure");
+    if ft.is_file() {
+      let path = de.path();
+      let path = RelativePathBuf::from_path(path).expect("path error");
+      files.push(path);
+    }
+  }
+
+  assert!(files.len() > 10);
+  assert!(files.contains(&RelativePathBuf::from("src/tree/walk.rs")));
 }
